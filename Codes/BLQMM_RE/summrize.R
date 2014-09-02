57c83b3f86e499f75b02d29f0962a4e666c3f580
askming <askming@gmail.com>
2014-09-02 10:54:30 -0500

    modified summerize code

--- Codes/BLQMM_RE/summrize.R
+++ Codes/BLQMM_RE/summrize.R
@@ -3,26 +3,33 @@
 ############################################################################
 ############################## simulated data ##############################
 ############################################################################
+rm(list=ls())
 setwd('/Users/askming/Dropbox/RA works/Self/Proposal writing/dataout')
 dir()
-load(dir()[4])
+load(dir()[5])
 
 # ---------- plot density for rando effects -------------------- #
-re1 = unlist(lapply(mn_t3_10_DPoutput4[[1]], `[[`, 'RE'))
-re2 = unlist(lapply(mn_t3_10_DPoutput4[[2]], `[[`, 'RE'))
-re3 = unlist(lapply(mn_t3_10_DPoutput4[[3]], `[[`, 'RE'))
+re1 = unlist(lapply(mn_t3_2re_10[[1]], `[[`, 're_int'))
+re2 = unlist(lapply(mn_t3_2re_10[[2]], `[[`, 're_int'))
+re3 = unlist(lapply(mn_t3_2re_10[[3]], `[[`, 're_int'))
 
-plot(density(re1))
+plot(density(re3))
 # lines(seq(-4,6, 0.01),dt(seq(-4,6, 0.01), 3))
+# lines(density(re2))
 
 # ---------- calculate bias for fixed effects -------------------- #
-beta = lapply(mn_t3_10_DPoutput4[[3]], `[[`, 'beta')
-beta0= unlist(lapply(beta, `[`, 1:3000, 1)) 
-beta1= unlist(lapply(beta, `[`, 1:3000, 2))
+bias = function(data, index, tau){
 
-bias0 = (mean(beta0)-(5+qt(0.75, 3)))/5
-bias1 = mean(beta1)-(-1+1/11*qt(0.75, 3))
-c(bias0, bias1)
+	beta = lapply(data[[index]], `[[`, 'beta')
+	beta0= unlist(lapply(beta, `[`, 1:3000, 1)) 
+	beta1= unlist(lapply(beta, `[`, 1:3000, 2))
+
+	bias0 = (mean(beta0)-(5+qt(tau[index], 3)))/5
+	bias1 = mean(beta1)-(-1+1/11*qt(tau[index], 3))
+	c(bias0, bias1)
+}
+
+bias(mn_t3_2re_10, 1, c(0.25, 0.5, 0.75))
 
 
 ############################################################################
@@ -31,9 +38,9 @@ c(bias0, bias1)
 setwd('/Users/askming/Dropbox/RA works/Self/Proposal writing/simulationdata')
 load(dir()[1])
 # ls()
-ui = lapply(mn_t3_2re_30, `[[`, 'ui1')
+ui = lapply(mn_t3_2re_30_new, `[[`, 'ui1')
 # par(mfrow=c(2,2))
-plot(density(ui[[1]]), ylim=c(0, 0.3), main='random intercept densities', sub="data=mn_t3_2re_30")
+plot(density(ui[[1]]), ylim=c(0, 1), main='random intercept densities', sub="data=mn_t3_2re_30")
 for (i in 2: 10){
 	lines(density(ui[[i]]), lty=i, col=i)
 }
