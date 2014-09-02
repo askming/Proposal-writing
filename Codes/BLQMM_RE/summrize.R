2721a0edcfc9481bb7e40355517517673d4a93ae
askming <askming@gmail.com>
2014-09-02 11:07:49 -0500

    revise summarize code

--- Codes/BLQMM_RE/summrize.R
+++ Codes/BLQMM_RE/summrize.R
@@ -1,21 +1,35 @@
 #------------ code to summarize simulation results -------------#
 
 ############################################################################
-############################## simulated data ##############################
+############################## sampled data ################################
 ############################################################################
 rm(list=ls())
 setwd('/Users/askming/Dropbox/RA works/Self/Proposal writing/dataout')
 dir()
 load(dir()[5])
 
-# ---------- plot density for rando effects -------------------- #
-re1 = unlist(lapply(mn_t3_2re_10[[1]], `[[`, 're_int'))
-re2 = unlist(lapply(mn_t3_2re_10[[2]], `[[`, 're_int'))
-re3 = unlist(lapply(mn_t3_2re_10[[3]], `[[`, 're_int'))
+# ---------- function to get posterior samples for rando effects -------------------- #
+get_res = function(post_data, re, qt){
+ 	# post_data is the posterior data
+ 	# re is the indicator of re_int or re_slope
+ 	# qt is the index of quantile, 1,2,3,...
+ 	r = unlist(lapply(post_data[[qt]], `[[`, re))
+ 	# r2 = unlist(lapply(post_data[[2]], `[[`, 're'))
+ 	# r3 = unlist(lapply(post_data[[3]], `[[`, 're'))
+ 	return(r)
+ 	
+}
 
-plot(density(re3))
-# lines(seq(-4,6, 0.01),dt(seq(-4,6, 0.01), 3))
-# lines(density(re2))
+re_int1 = get_res(mn_t3_2re_10, 're_int', 1)
+re_int2 = get_res(mn_t3_2re_10, 're_int', 2)
+re_int3 = get_res(mn_t3_2re_10, 're_int', 3)
+
+re_slope1 = get_res(mn_t3_2re_10, 're_slope', 1)
+re_slope2 = get_res(mn_t3_2re_10, 're_slope', 2)
+re_slope3 = get_res(mn_t3_2re_10, 're_slope', 3)
+
+
+
 
 # ---------- calculate bias for fixed effects -------------------- #
 bias = function(data, index, tau){
@@ -38,9 +52,25 @@ bias(mn_t3_2re_10, 1, c(0.25, 0.5, 0.75))
 setwd('/Users/askming/Dropbox/RA works/Self/Proposal writing/simulationdata')
 load(dir()[1])
 # ls()
-ui = lapply(mn_t3_2re_30_new, `[[`, 'ui1')
-# par(mfrow=c(2,2))
-plot(density(ui[[1]]), ylim=c(0, 1), main='random intercept densities', sub="data=mn_t3_2re_30")
-for (i in 2: 10){
-	lines(density(ui[[i]]), lty=i, col=i)
+# ---------- function to plot the density of rando effects -------------------- #
+plot_data = function(data, re, ...){
+ 	# data is the original simulated data
+ 	# re is the indicator of re_int or re_slope
+ 	if (re == 're_int') var = 'ui1'
+ 	if (re == 're_slope') var = 'ui2'	
+ 	ui = lapply(data, `[[`, var)
+ 	plot(density(unlist(ui)), ...)	
 }
+
+#### try ####
+
+plot_data(mn_t3_2re_30, 're_int', ylim=c(0, 0.3), main='random intercept densities', sub="data=mn_t3_2re_30")
+lines(density(re_int1+0.5), lty=2, col=2)
+lines(density(re_int2+0.5), lty=3, col=3)
+lines(density(re_int3+0.5), lty=4, col=4)
+
+# lines(density(re_slope1), lty=2, col=2)
+# lines(density(re_slope2), lty=3, col=3)
+# lines(density(re_slope3), lty=4, col=4)
+
+legend('topright', legend=c("empirical data", "tau=0.25", "tau=0.5", "tau=0.75"), col=c(1, 2, 3, 4), lty=c(1,2,3, 4), cex=0.75)
